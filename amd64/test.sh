dbf9eb40cca54879377f2966b0e1990206028afb
Valdis <valdisxp1@gmail.com>
2017-07-28 22:43:25 +0300

    TRUST=yes/all option to disable server key checking

--- amd64/test.sh
+++ amd64/test.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+if [[ "$TRUST" = "YES" ]] || [[ "$TRUST" = "yes" ]] || [[ "$TRUST" = "ALL" ]] || [[ "$TRUST" = "all" ]]
+then
+	EXTRAOPTS="-o StrictHostKeyChecking=no"
+fi
+
 echo "Testing the connection"
 if [[ -z $PORT ]]
 then
-	ssh $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST"
+	ssh $EXTRAOPTS $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST"
 else
-	ssh -p $PORT $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST:$PORT"
+	ssh $EXTRAOPTS -p $PORT $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST:$PORT"
 fi
 
