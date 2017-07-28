6d3ae6c2b96fb0c5adbf7f966f1422cb18a190f1
Valdis <valdisxp1@gmail.com>
2017-07-28 17:28:11 +0300

    only generate missing keys

--- amd64/keygen.sh
+++ amd64/keygen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-ssh-keygen -t rsa -b 4096 -N '' -C "generated key" -f /root/.ssh/id_rsa
+if ! ls /root/.ssh/id_rsa 1> /dev/null 2>&1
+then
+	ssh-keygen -t rsa -b 4096 -N '' -C "generated key" -f /root/.ssh/id_rsa
+fi
 echo "-------- PUBLIC SSH KEY --------"
 cat /root/.ssh/id_rsa.pub
 echo "-------- PUBLIC SSH KEY END --------"
