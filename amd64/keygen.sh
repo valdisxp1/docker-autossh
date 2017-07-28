e5ae6b6bbe41a81a34726e4039e8e76db8d93c25
Valdis <valdisxp1@gmail.com>
2017-07-28 17:21:45 +0300

    keygen

--- amd64/keygen.sh
+++ amd64/keygen.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-
+ssh-keygen -t rsa -b 4096 -N '' -C "generated key" -f /root/.ssh/id_rsa
+echo "-------- PUBLIC SSH KEY --------"
+cat /root/.ssh/id_rsa.pub
+echo "-------- PUBLIC SSH KEY END --------"
