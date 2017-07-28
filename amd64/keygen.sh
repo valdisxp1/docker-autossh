aff24fbe79d63742e90ab461e6a21d10453866e1
Valdis <valdisxp1@gmail.com>
2017-07-28 19:54:57 +0300

    use user home not obsolute path

--- amd64/keygen.sh
+++ amd64/keygen.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-if ! ls /root/.ssh/id_rsa 1> /dev/null 2>&1
+
+if ! ls ~/.ssh/id_rsa 1> /dev/null 2>&1
 then
-	ssh-keygen -t rsa -b 4096 -N '' -C "generated key" -f /root/.ssh/id_rsa
+	ssh-keygen -t rsa -b 4096 -N '' -C "generated key" -f ~/.ssh/id_rsa
 fi
 echo "-------- PUBLIC SSH KEY --------"
-cat /root/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub
 echo "-------- PUBLIC SSH KEY END --------"
--- amd64/test.sh
+++ amd64/test.sh
@@ -3,8 +3,8 @@
 echo "Testing the connection"
 if [[ -z $PORT ]]
 then
-	ssh $HOST -i /root/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST"
+	ssh $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST"
 else
-	ssh -p $PORT $HOST -i /root/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST:$PORT"
+	ssh -p $PORT $HOST -i ~/.ssh/id_rsa echo 'Can connect to $USER@$HOSTNAME' "via $HOST:$PORT"
 fi
 
