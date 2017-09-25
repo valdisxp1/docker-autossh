815b096e22b95c7318eb632bc516677281d8c9d6
Valdis <valdisxp1@gmail.com>
2017-09-25 13:59:14 +0200

    do not go into background

--- armv7/tunnel.sh
+++ armv7/tunnel.sh
@@ -2,9 +2,4 @@
 
 echo "Starting autossh tunnel"
 
-autossh -M 0 -f -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
-
-while [[ 1 ]]
-do
-	sleep 60
-done
+autossh -M 0 -N -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
