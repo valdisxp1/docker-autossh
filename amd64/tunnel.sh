8000b38403ba71ec63b616b6ddd8e78d234d9c15
Valdis <valdisxp1@gmail.com>
2017-09-25 16:42:25 +0200

    verbose option

--- amd64/tunnel.sh
+++ amd64/tunnel.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [[ "$VERBOSE" = "YES" ]] || [[ "$VERBOSE" = "yes" ]]
+then
+	VERBOSE_FLAG="-vv"
+fi
+
 echo "Starting autossh tunnel"
 
-autossh -M 0 -N -vv -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
+autossh -M 0 -N $VERBOSE_FLAG -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
