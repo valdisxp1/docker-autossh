82fb587ab41077a8a633412c8a5b8eaf67105104
Valdis <valdisxp1@gmail.com>
2017-10-11 22:26:06 +0200

    fail and retry if the tunneling fails even if the ssh does not

--- amd64/tunnel.sh
+++ amd64/tunnel.sh
@@ -7,4 +7,4 @@ fi
 
 echo "Starting autossh tunnel"
 
-autossh -M 0 -N $VERBOSE_FLAG -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
+autossh -M 0 -N $VERBOSE_FLAG -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" $FORWARD -i ~/.ssh/id_rsa -p $PORT $HOST
